0e2c8213a710aa61c782537e75407e3ff439b5aa
Rabbadhaki <badha.ron09@gmail.com>
2025-09-25 15:52:14 +0300

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -22,11 +22,10 @@ def main():
 
     # Part 2: Ask user for days and calculate future date
     try:
-        days = int(input("Enter number of days to add: "))
+        days = int(input("Enter the number of days to add to the current date: "))
         calculate_future_date(days)
     except ValueError:
         print("Invalid input. Please enter an integer.")
 
 if __name__ == "__main__":
     main()
-
